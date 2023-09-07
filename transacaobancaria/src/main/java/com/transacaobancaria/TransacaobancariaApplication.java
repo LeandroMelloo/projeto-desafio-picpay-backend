f28dbf6bba1a85c4962c3ae0a639817987d806e7
LeandroMelloo <leandromoreno1909@hotmail.com>
2023-09-06 21:25:59 -0300

    feat: initial config

---
+++ transacaobancaria/src/main/java/com/transacaobancaria/TransacaobancariaApplication.java
@@ -0,0 +1,13 @@
+package com.transacaobancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaobancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaobancariaApplication.class, args);
+	}
+
+}
---
+++ transacaobancaria/src/test/java/com/transacaobancaria/TransacaobancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transacaobancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaobancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
